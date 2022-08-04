c6314fa71c74468653d585e56d1df6c39db9a81e
NikhilChavan0104 <nikhilchavan0104@gmail.com>
2022-08-04 17:40:07 +0530

    c1

---
+++ Collection1/src/collection1/Yogiraj.java
@@ -0,0 +1,14 @@
+package collection1;
+
+public class Yogiraj {
+
+	int a=10;
+	public static void main(String[] args) {
+//System.out.println(true>false);
+		Yogiraj s=new Yogiraj();
+		Yogiraj ss=new Yogiraj();
+		//System.out.println(s>ss);
+
+	}
+
+}
